675342c7681babe0e4ba2e8c300a0f061a1c38c2
SprikerS <jeremyestelo17@gmail.com>
2025-06-25 19:42:11 -0500

    feat: update home page layout and add login link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import { ThemeToggle } from '@/components/ui'
+import Link from 'next/link'
+
+import { buttonVariants, ThemeToggle } from '@/components/ui'
 
 export default function Home() {
   return (
-    <div className="grid h-screen place-items-center">
+    <div className="flex h-screen flex-col items-center justify-center gap-10">
       <ThemeToggle />
+      <h1 className="text-2xl font-bold">Authentication</h1>
+      <Link href="/login" className={buttonVariants({ variant: 'secondary' })}>
+        Login
+      </Link>
     </div>
   )
 }
