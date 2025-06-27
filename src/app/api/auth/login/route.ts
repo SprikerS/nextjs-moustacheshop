1b5fc6440ed3a989be60f6ae885976a699f2b298
SprikerS <jeremyestelo17@gmail.com>
2025-06-27 12:40:38 -0500

    feat: update the login api url to use the environment variable

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers'
 
 export async function POST(req: Request) {
   const body = await req.text()
-  const res = await fetch('http://localhost:3003/api/user/login', {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body,
