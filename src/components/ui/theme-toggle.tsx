99554f74fbfb487840a729d1cfc7a2f6dab97991
SprikerS <jeremyestelo17@gmail.com>
2025-06-26 21:37:15 -0500

    feat: add all shadcn ui components to the project

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -13,12 +13,7 @@ const ThemeToggle = () => {
   }
 
   return (
-    <Button
-      onClick={toggleTheme}
-      className="cursor-pointer size-8"
-      size="icon"
-      variant="ghost"
-    >
+    <Button onClick={toggleTheme} className="cursor-pointer size-8" size="icon" variant="ghost">
       <SunIcon className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
