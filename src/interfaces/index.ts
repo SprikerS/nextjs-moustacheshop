32248f075cd5599e3c74313b7313cef6f1d62364
SprikerS <jeremyestelo17@gmail.com>
2025-07-01 16:03:52 -0500

    refactor: add user interface definition to the interfaces module

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,6 +5,21 @@ export interface PaginatedResponse<T> {
   offset: number
 }
 
+export type Role = 'customer' | 'employee' | 'admin' | 'super-user'
+
+export interface User {
+  id: string
+  names: string
+  paternalSurname: string
+  maternalSurname: string
+  dni: string
+  email: string
+  phoneNumber?: number
+  active: boolean
+  roles: Role[]
+  token: string
+}
+
 export interface Category {
   id: string
   name: string
