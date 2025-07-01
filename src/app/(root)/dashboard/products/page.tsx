71b7371a8ebcf4d38ff9a4c29e17718aef954833
SprikerS <jeremyestelo17@gmail.com>
2025-07-01 08:58:40 -0500

    refactor: organize the product page component structure to improve readability

--- src/app/(root)/dashboard/products/page.tsx
+++ src/app/(root)/dashboard/products/page.tsx
@@ -7,7 +7,7 @@ import type { SearchParams } from 'nuqs/server'
 import { CATEGORY_ACTIONS, PRODUCT_ACTIONS } from '@/actions'
 import { productSearchParams, ProductsTable, ProductsTableLoading } from '@/components/products'
 import { TableFacetedFilter, TableFilter, TableToggleFilter } from '@/components/shared/table'
-import { Button, Card, CardContent } from '@/components/ui'
+import { Button, Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui'
 
 type PageProps = {
   searchParams: Promise<SearchParams>
@@ -20,6 +20,20 @@ export default async function ProductsPage({ searchParams }: PageProps) {
   return (
     <div className="flex flex-col gap-5">
       <Card className="container mx-auto bg-transparent">
+        <CardHeader>
+          <CardTitle>Productos</CardTitle>
+          <CardDescription>
+            Administra los productos de tu tienda, crea nuevos productos, edita o elimina los existentes
+          </CardDescription>
+          <CardAction>
+            <Link href="/dashboard/products/create">
+              <Button>
+                <PackagePlus />
+                Crear producto
+              </Button>
+            </Link>
+          </CardAction>
+        </CardHeader>
         <CardContent className="flex flex-col gap-6">
           <div className="flex justify-between gap-6">
             <div className="flex items-center gap-3 w-full">
@@ -30,19 +44,11 @@ export default async function ProductsPage({ searchParams }: PageProps) {
               <TableFacetedFilter categories={categories} refetch={PRODUCT_ACTIONS.revalidate} />
               <TableToggleFilter refetch={PRODUCT_ACTIONS.revalidate} />
             </div>
-            <div className="flex items-center gap-3">
-              <Link href="/dashboard/products">
-                <Button variant="ghost">
-                  <RotateCcw />
-                </Button>
-              </Link>
-              <Link href="/dashboard/products/create">
-                <Button>
-                  <PackagePlus />
-                  Crear producto
-                </Button>
-              </Link>
-            </div>
+            <Link href="/dashboard/products">
+              <Button variant="ghost" size="sm" className="h-8">
+                <RotateCcw />
+              </Button>
+            </Link>
           </div>
           <Suspense fallback={<ProductsTableLoading />}>
             <ProductsTable params={params} />
