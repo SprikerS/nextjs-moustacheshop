2a4019f07ef52aa183eeed99407e00de314b68b8
SprikerS <jeremyestelo17@gmail.com>
2025-07-07 19:29:10 -0500

    feat: implement order fetching and redirection in edit product page

--- src/app/(root)/dashboard/sales/[id]/page.tsx
+++ src/app/(root)/dashboard/sales/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { redirect } from 'next/navigation'
+
 import { SaleForm } from '@/components/sales'
+import { ORDER_ACTIONS } from '@/actions'
 
 type Params = Promise<{ id: string }>
 
@@ -6,9 +9,14 @@ export default async function EditProductPage(props: { params: Params }) {
   const params = await props.params
   const { id } = params
 
+  const { data, error } = await ORDER_ACTIONS.findOneById(id)
+  if (!data || error) {
+    redirect('/dashboard/sales')
+  }
+
   return (
     <>
-      <SaleForm />
+      <SaleForm order={data} />
     </>
   )
 }
